6cb27a5a253dbcea8f74b33a073badcc60db0f70
Sean <sean.feeney@quinnipiac.edu>
2015-12-03 14:49:53 +0000

    update view

--- club_lax/settings.py
+++ club_lax/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'bootstrap3',
 )
 
 MIDDLEWARE_CLASSES = (
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, ListView
+from django.views.generic import TemplateView, CreateView, ListView, DetailView, UpdateView
 from django.core.urlresolvers import reverse_lazy
 from .models import *
 
@@ -8,16 +8,24 @@ class Home(TemplateView):
   template_name = "home.html"
 
 class QuestionCreateView(CreateView):
-    model = Question
-    template_name = "question/question_form.html"
-    fields = ['title', 'description', 'visibility']
-    success_url = reverse_lazy('question_list')
+  model = Question
+  template_name = "question/question_form.html"
+  fields = ['title', 'description', 'visibility']
+  success_url = reverse_lazy('question_list')
 
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super(QuestionCreateView, self).form_valid(form)
-      
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super(QuestionCreateView, self).form_valid(form)
+    
 class QuestionListView(ListView):
+  model = Question
+  template_name = "question/question_list.html"
+
+class QuestionDetailView(DetailView):
+  model = Question
+  template_name = "question/question_detail.html"
+  
+class QuestionUpdateView(UpdateView):
     model = Question
-    template_name = "question/question_list.html"
-   
+    template_name = 'question/question_form.html'
+    fields = ['title', 'description']
