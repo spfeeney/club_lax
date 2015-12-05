4c0142379bfdb8c69cbbe27f9afbe3ef992f3c29
Sean <sean.feeney@quinnipiac.edu>
2015-12-05 22:07:43 +0000

    answer update

--- core/views.py
+++ core/views.py
@@ -28,6 +28,13 @@ class QuestionDetailView(DetailView):
   model = Question
   template_name = "question/question_detail.html"
 
+  def get_context_data(self, **kwargs):
+      context = super(QuestionDetailView, self).get_context_data(**kwargs)
+      question = Question.objects.get(id=self.kwargs['pk'])
+      answers = Answer.objects.filter(question=question)
+      context['answers'] = answers
+      return context
+
 class QuestionUpdateView(UpdateView):
   model = Question
   template_name = 'question/question_form.html'
@@ -50,3 +57,14 @@ class AnswerCreateView(CreateView):
         form.instance.user = self.request.user
         form.instance.question = Question.objects.get(id=self.kwargs['pk'])
         return super(AnswerCreateView, self).form_valid(form)
+      
+class AnswerUpdateView(UpdateView):
+    model = Answer
+    pk_url_kwarg = 'answer_pk'
+    template_name = 'answer/answer_form.html'
+    fields = ['text']
+
+    def get_success_url(self):
+        return self.object.question.get_absolute_url()
+
+  
